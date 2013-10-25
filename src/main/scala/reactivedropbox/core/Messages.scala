1f33d2fb575880ee43df0b56eb764a23c619df26
Roy van Kaathoven <royvankaathoven@hotmail.com>
2013-10-26 00:42:05 +0200

    docs

--- src/main/scala/reactivedropbox/core/Messages.scala
+++ src/main/scala/reactivedropbox/core/Messages.scala
@@ -9,12 +9,36 @@ import java.io.File
  */
 case class AppInfo(key: String, secret: String)
 
-case class AddFile(source: String, target: String, writeMode: DbxWriteMode = DbxWriteMode.add())
+/**
+ * Add a new file
+ * @param localPath local filepath
+ * @param remotePath remote filepath
+ * @param writeMode add, overwrite or specify a revision to add
+ */
+case class AddFile(localPath: String, remotePath: String, writeMode: DbxWriteMode = DbxWriteMode.add())
 
+/**
+ * Wrapper around dropbox entry
+ * @param entry dropbox entry
+ */
 case class Entry(entry: DbxEntry)
 
+/**
+ * Local file with related dropbox entry
+ * @param path local file path
+ * @param entry dropbox entry
+ */
 case class LocalFile(path: String, entry: DbxEntry) {
+  /**
+   * @return java.io.File of the local filepath
+   */
   def toFile = new File(path)
 }
 
+/**
+ * Download file to local path
+ * @param remotePath Remote path to download
+ * @param localPath Local path to download
+ * @param revision optional revision which to download
+ */
 case class DownloadFile(remotePath: String, localPath: String, revision: Option[String] = None)
